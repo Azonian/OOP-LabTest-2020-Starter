0c6f3641f69c194ff173c240d9d54362b3108c97
Azonian <42889044+Azonian@users.noreply.github.com>
2020-04-22 13:03:16 +0100

    add vertical lines and number lables

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -37,10 +37,38 @@ public class Gantt extends PApplet
 		println("Mouse dragged");
 	}
 
-	
+	public void drawLabledMarkers() {
+		/*
+		using a ruler heres the aprox distaces ratios of the screen:
+		
+		witdth:42
+		highth:31
+		each gap distace: 1
+		top+bottom+right buffer:2
+		left buffer 8
+		text:midle is at 1 from top
+		*/
+		
+		float border = width * 0.05f;
+		float leftBorder = width * 0.2f;
+		/*using 0.05f rather than 0.04761904761904761904761904761905
+		like the math sugests cause im assuming it was a mesurment error
+		and that indeded value was 0.05*/
+		
+		stroke(100,100,100);
+		textAlign(CENTER,CENTER);
+		for(int i = 1;i <= 30;i++)
+		{
+			float xCord = map(i,1,30,leftBorder,width - border);
+			line(xCord,border,xCord,height - border);
+			
+			text(i,xCord,border/2);
+		}
+	}
 	
 	public void setup() 
 	{
+		//colorMode()
 		loadTasks();
 		printTasks();
 	}
@@ -56,6 +84,6 @@ public class Gantt extends PApplet
 	public void draw()
 	{			
 		background(0);
-		//drawLabledMarkers();
+		drawLabledMarkers();
 	}
 }
